227b1bcfce74c88c88102d8779f489395e5b7c79
vpa24 <vuphuonganh020497@gmail.com>
2019-09-18 22:12:40 +0700

    popup after login

---
+++ js/popup.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  if ($.cookie("isPopup") == 0 || $.cookie("isPopup") === undefined) {
+    var baseUrl = Drupal.settings.baseUrl;
+    var url = `${baseUrl}/node/1`;
+    $("#myModal").modal({
+      backdrop: false
+    });
+    $("#btnAgree").click(function() {
+      $.ajax({
+        type: "POST",
+        url: "?q=mymodule_ajax",
+        data: {}
+      }).done(function() {
+        $(location).attr("href", url);
+      });
+    });
+  }
+});
