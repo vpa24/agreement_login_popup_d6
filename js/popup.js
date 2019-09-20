b1d848910338583e7a81f11fb8fd1c4af5667eec
vpa24 <vuphuonganh020497@gmail.com>
2019-09-20 18:23:32 +0700

    set logout button

--- js/popup.js
+++ js/popup.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
-  if ($.cookie("isPopup") == 0 || $.cookie("isPopup") === undefined) {
-    var baseUrl = Drupal.settings.baseUrl;
-    var url = `${baseUrl}/node/1`;
-    $("#myModal").modal({
-      backdrop: false
-    });
-    $("#btnAgree").click(function() {
-      $.ajax({
-        type: "POST",
-        url: "?q=mymodule_ajax",
-        data: {}
-      }).done(function() {
-        $(location).attr("href", url);
-      });
-    });
-  }
+	if ($.cookie("isPopup") == 0 || $.cookie("isPopup") === undefined) {
+		var baseUrl = Drupal.settings.baseUrl;
+		var url = `${baseUrl}/node/1`;
+		var logoutbtn = `${baseUrl}`;
+		
+		$("#myModal").modal({
+		  backdrop: false
+		});
+		$("#btnAgree").click(function() {
+		  $.ajax({
+			type: "POST",
+			url: "?q=mymodule_ajax",
+			data: {}
+		  }).done(function() {
+			$(location).attr("href", url);
+		  });
+		});
+		$("#btnDisagree").click(function() {
+			$(location).attr(logoutbtn);
+		});	
+	}
 });
