ae6249a5a7d5c4009d470178a91b22da91f05897
vpa24 <vuphuonganh020497@gmail.com>
2019-10-01 14:29:28 +0700

    don't use jquery cookie

--- js/popup.js
+++ js/popup.js
@@ -1,23 +1,24 @@
 $(document).ready(function() {
-	if ($.cookie("isPopup") == 0 || $.cookie("isPopup") === undefined) {
-		var baseUrl = Drupal.settings.baseUrl;
-		var url = `${baseUrl}/node/1`;
-		var logoutbtn = `${baseUrl}`;
-		
-		$("#myModal").modal({
-		  backdrop: false
-		});
-		$("#btnAgree").click(function() {
-		  $.ajax({
-			type: "POST",
-			url: "?q=mymodule_ajax",
-			data: {}
-		  }).done(function() {
-			$(location).attr("href", url);
-		  });
-		});
-		$("#btnDisagree").click(function() {
-			$(location).attr(logoutbtn);
-		});	
-	}
+  var baseUrl = Drupal.settings.baseUrl;
+  var isPopup = Drupal.settings.isPopup;
+  if (isPopup) {
+    var logoutbtn = `${baseUrl}/logout`;
+
+    $("#myModal").modal({
+      backdrop: false
+    });
+    $("#btnAgree").click(function() {
+      $.ajax({
+        type: "POST",
+        url: "?q=mymodule_ajax",
+        data: {}
+      }).done(function() {
+        $("#myModal").modal("hide");
+        isPopup = false;
+      });
+    });
+    $("#btnDisagree").click(function() {
+      $(location).attr(logoutbtn);
+    });
+  }
 });
